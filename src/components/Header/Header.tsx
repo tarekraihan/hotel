7caf4b4e974461eb7e9edb7216eb6c6ac826d5a2
Tarek Raihan <ziat@springrain.io>
2024-01-08 00:07:41 +0600

    Implement theme swtching

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
+"use client";
+import ThemeContext from "@/context/themeContext";
 import Link from "next/link";
+import { useContext } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { MdDarkMode } from "react-icons/md";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 const Header = () => {
+  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
   return (
     <header className="py-10 px-10 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-center">
       <div className="flex items-center w-full md:2/3">
@@ -15,7 +19,23 @@ const Header = () => {
             </Link>
           </li>
           <li className="ml-2">
-            <MdDarkMode className="cursor-pointer" />
+            {darkTheme ? (
+              <MdOutlineLightMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(false);
+                  localStorage.removeItem("hotel-theme");
+                }}
+              />
+            ) : (
+              <MdDarkMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(true);
+                  localStorage.setItem("hotel-theme", "true");
+                }}
+              />
+            )}
           </li>
         </ul>
       </div>
